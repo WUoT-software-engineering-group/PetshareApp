2c2f517ab974931a904979e1453e9bf1ed71186a
Sebastian Szarafin <01161541@pw.edu.pl>
2023-03-18 17:03:42 +0100

    add db schema

---
+++ Src/Domain/Entities/Annoucement.cs
@@ -0,0 +1,54 @@
+﻿namespace Petshare.Domain.Entities;
+
+public enum AnnoucementStatus
+{
+    Open,
+    Closed,
+    Verification
+}
+
+public class Annoucement
+{
+    public Guid ID { get; private set; }
+    public Shelter Author { get; private set; }
+    public Pet Pet { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public DateTime CreationDate { get; private set; }
+    public DateTime ClosingDate { get; private set; }
+    public AnnoucementStatus Status { get; set; }
+
+    public Annoucement(Shelter author, Pet pet, string title, string description, DateTime creationDate, DateTime closingDate)
+    {
+        ID = Guid.NewGuid();
+        Author = author;
+        Pet = pet;
+        Title = title;
+        Description = description;
+        CreationDate = creationDate;
+        ClosingDate = closingDate;
+        Status = AnnoucementStatus.Open;
+    }
+
+    public Annoucement() { }
+}
+
+public abstract class AdopterAnnoucement
+{
+    public Guid AdopterID { get; private set; }
+    public Adopter Adopter { get; private set; }
+    public Guid AnnoucementID { get; private set; }
+    public Annoucement annoucement { get; private set; }
+
+    public AdopterAnnoucement() { }
+}
+
+public class AdopterAnnoucementFollowed : AdopterAnnoucement
+{
+    public AdopterAnnoucementFollowed() { }
+}
+
+public class AdopterAnnoucementFinalised : AdopterAnnoucement
+{
+    public AdopterAnnoucementFinalised() { }
+}
---
+++ Src/Petshare.WebAPI/Data/PetshareDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Petshare.WebAPI.Data;
+
+public class PetshareDbContextFactory : IDesignTimeDbContextFactory<PetshareDbContext>
+{
+    public PetshareDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<PetshareDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseSqlServer(connectionString, b => b.MigrationsAssembly("Petshare.WebAPI"));
+
+
+        return new PetshareDbContext(builder.Options);
+    }
+}
