3c701d40ec6d5f5054c7921e0526043115897f73
Jakub Urbański <kuba.urban01@gmail.com>
2023-03-28 17:25:25 +0200

    PET-77: add announcements controller

---
+++ Src/Petshare.Presentation/Controllers/AnnouncementController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petshare.Services.Abstract;
+
+namespace Petshare.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnnouncementController : ControllerBase
+    {
+        private readonly IServiceWrapper _serviceWrapper;
+
+        public AnnouncementController(IServiceWrapper serviceWrapper)
+        {
+            _serviceWrapper = serviceWrapper;
+        }
+    }
+}
