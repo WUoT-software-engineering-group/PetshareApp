6f729a597e0625545c30115fc33fcbc618b41400
Jakub Urbański <kuba.urban01@gmail.com>
2023-03-29 20:34:46 +0200

    PET-72: merge announcement unit tests files

--- Src/Petshare.Services.Abstract/IAnnouncementService.cs
+++ Src/Petshare.Services.Abstract/IAnnouncementService.cs
@@ -5,7 +5,6 @@ namespace Petshare.Services.Abstract;
 public interface IAnnouncementService
 {
     Task<AnnouncementResponse?> Create(Guid shelterId, PostAnnouncementRequest announcement);
-
     Task<bool> Update(Guid userId, Guid announcementId, PutAnnouncementRequest announcement);
     Task<AnnouncementResponse?> GetById(Guid announcementId);
     Task<List<AnnouncementResponse>> GetByFilters(GetAnnouncementsRequest filters);
