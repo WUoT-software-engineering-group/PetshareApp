bf040f4da8eb1e22810a37bbadd24346e5520555
Sebastian Szarafin <01161541@pw.edu.pl>
2023-03-31 18:37:30 +0200

    move db secrets to azure key vault

--- Src/Petshare.WebAPI/Program.cs
+++ Src/Petshare.WebAPI/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using Petshare.DataPersistence;
+using Azure.Identity;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Petshare.WebAPI;
 
@@ -16,5 +17,22 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args)
         => Host.CreateDefaultBuilder(args)
-               .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+               .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+               .ConfigureAppConfiguration((context, config) =>
+               {
+                   var builtConfig = config.Build();
+
+                   var client = new KeyVaultClient((authority, resource, scope) =>
+                   {                       
+                       var credential = new DefaultAzureCredential(false);
+                       return Task.FromResult(credential.GetToken(
+                           new Azure.Core.TokenRequestContext(
+                               new[] {"https://vault.azure.net/.default"})).Token);
+                   });
+
+                   config.AddAzureKeyVault(
+                       builtConfig["VaultUri"],
+                       client, 
+                       new DefaultKeyVaultSecretManager());
+               });
 }
